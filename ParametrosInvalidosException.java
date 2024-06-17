7cefba602a7aa99c45ab6a83286c5f99e31fb7c9
Anderson-Alves <anderson.dmc07@gmail.com>
2024-06-17 13:13:25 -0300

    Finalizado

---
+++ ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+// ParametrosInvalidosException.java (Exceção)
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+}
+
